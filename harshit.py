ac6ee57b0374b19f955be41794ea62acd3933f4f
ShubhamBishnoi29 <52317533+ShubhamBishnoi29@users.noreply.github.com>
2021-01-11 13:47:36 +0530

    Update harshit.py

--- harshit.py
+++ harshit.py
@@ -4,8 +4,4 @@ print("hii buddy")
 print("hii anuj darling")
 print("hiii anuj the legend")
 print("My name is Shubham bishnoi")
-<<<<<<< HEAD
 print("Noe I am in Anuj branch")
-=======
-
->>>>>>> a06bca5c267947313007e98e3aeb7c0af7c11afa
